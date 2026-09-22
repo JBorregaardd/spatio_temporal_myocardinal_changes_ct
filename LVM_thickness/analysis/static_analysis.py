bc06548628b25d7212ddc92ae0ea1fd6d7c1b7dd
SigneDjernis <signedjernis@gmail.com>
2026-09-22 10:20:51 +0200

    changed from mean to median

--- LVM_thickness/analysis/static_analysis.py
+++ LVM_thickness/analysis/static_analysis.py
@@ -1,3 +1,8 @@
+#################################################################################################################################################
+# This script takes as input the output folder from the LVM thickness pipeline and calculates the median thickness for each of the 17 segments.
+# It saves the results to a CSV file in the output folder.
+#################################################################################################################################################
+
 import os
 import csv
 import argparse
@@ -56,7 +61,7 @@ def get_scalar_ring_mm_coordinates(total_path, mesh_name):
     return scalar_image
 
 
-def calculate_mean_thickness_per_segment(folder, mesh_name, total_path):
+def calculate_median_thickness_per_segment(folder, mesh_name, total_path):
 
     # Get thickness values on the myocardium ring
     scalar_image = get_scalar_ring_mm_coordinates(total_path, mesh_name)
@@ -97,7 +102,7 @@ def calculate_mean_thickness_per_segment(folder, mesh_name, total_path):
     segments = sitk.GetArrayFromImage(lv17_transformed).transpose(2, 1, 0)
     thickness = sitk.GetArrayFromImage(scalar_seg).transpose(2, 1, 0)
 
-    # Calculate mean thickness for each segment
+    # Calculate median thickness for each segment
     results = []
 
     for segment in range(1, 18):
@@ -108,13 +113,13 @@ def calculate_mean_thickness_per_segment(folder, mesh_name, total_path):
         values = values[values > 0]
 
         if len(values) > 0:
-            mean_thickness = np.mean(values)
+            median_thickness = np.median(values)
         else:
-            mean_thickness = np.nan
+            median_thickness = np.nan
 
         results.append({
             "segment": segment,
-            "mean_thickness": mean_thickness
+            "median_thickness": median_thickness
         })
 
     return results
@@ -141,16 +146,16 @@ if __name__ == "__main__":
     mesh_name = os.path.join(folder, "surfaces", "dist_source.vtk")
     total_path = os.path.join(root, "data", "TotalSegmentator", f"{test_patient_id}.heart.nii.gz")
 
-    table = calculate_mean_thickness_per_segment(folder, mesh_name, total_path)
+    table = calculate_median_thickness_per_segment(folder, mesh_name, total_path)
 
     # add patient id to the table
     for row in table:
         row["patient_id"] = test_patient_id
 
     # Save the results to a CSV file
-    output_csv_path = os.path.join(folder, "mean_thickness_per_segment.csv")
+    output_csv_path = os.path.join(folder, "median_thickness_per_segment.csv")
 
     with open(output_csv_path, "w", newline="") as f:
-        writer = csv.DictWriter(f, fieldnames=["patient_id", "segment", "mean_thickness"])
+        writer = csv.DictWriter(f, fieldnames=["patient_id", "segment", "median_thickness"])
         writer.writeheader()
         writer.writerows(table)
